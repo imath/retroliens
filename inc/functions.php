994c01dc8bcd1e16068864561f0440bf5a66cb7c
imath <math.viet@gmail.com>
2020-01-09 17:32:28 +0100

    Introduce Trackbacks Management into the sidebar

--- inc/functions.php
+++ inc/functions.php
@@ -40,46 +40,110 @@ function retroliens_enqueue_script() {
 add_action( 'enqueue_block_editor_assets', 'retroliens_enqueue_script' );
 
 /**
- * Fetches the `to_ping` post field.
+ * Fetches the trackback post fields.
  *
  * @since 1.0.0
  *
- * @param WP_Post $post The post object.
- * @return array        The list of trackbacks to ping.
+ * @param array $prepared Prepared response array.
+ * @return array          The list of trackbacks to ping.
  */
-function retroliens_get_to_ping( $post ) {
-	$to_ping    = array();
-	$trackbacks = get_post_field( 'to_ping', $post->ID );
+function retroliens_get_trackbacks( $prepared, $field ) {
+	$retval = array();
 
-	if ( $trackbacks ) {
-		$urls    = trim( $trackbacks, "\n" );
-		$to_ping = explode( "\n", $urls );
+	if ( ! $prepared ) {
+		return $retval;
 	}
 
-	return $to_ping;
+	if ( is_object( $prepared ) ) {
+		$prepared = (array) $prepared;
+	}
+
+	if ( ! isset( $prepared['id'] ) ) {
+		return $retval;
+	}
+
+	$trackbacks = get_post_field( $field, $prepared['id'] );
+
+	if ( $trackbacks ) {
+		$urls   = trim( $trackbacks, "\n" );
+		$retval = explode( "\n", $urls );
+	}
+
+	return $retval;
 }
 
 /**
- * Sanitizes the `to_ping` field.
+ * Formats trackbacks list the way WordPress expects it.
+ *
+ * @since 1.0.0
+ *
+ * @param array $trackbacks The list of trackbacks.
+ * @return string The space separated list of trackbacks.
+ */
+function retroliens_prepare_trackbacks( $trackbacks ) {
+	if ( $trackbacks && is_array( $trackbacks ) ) {
+		return implode( ' ', $trackbacks );
+	}
+
+	return '';
+}
+
+/**
+ * Sanitizes the trackbacks.
  *
  * @since 1.0.0
  *
  * @param string|array $trackbacks The trackbacks to ping for the post.
  * @return string                  The sanitized trackbacks to ping for the post.
  */
-function retroliens_sanitize_to_ping( $trackbacks ) {
-	$to_ping = '';
+function retroliens_sanitize_trackbacks( $trackbacks ) {
+	$retval = retroliens_prepare_trackbacks( $trackbacks );
 
-	if ( ! $trackbacks ) {
-		return $to_ping;
+	if ( $retval ) {
+		$retval = sanitize_trackback_urls( $retval );
 	}
 
-	if ( is_array( $trackbacks ) ) {
-		$to_ping = implode( ' ', $trackbacks );
-	}
-
-	return sanitize_trackback_urls( $to_ping );
+	return $retval;
 }
+
+/**
+ * Overloads the prepared post object with `to_ping` and `pinged` properties when needed.
+ *
+ * @since 1.0.0
+ *
+ * @param stdClass        $prepared_post An object representing a single post prepared
+ *                                       for inserting or updating the database.
+ * @param WP_REST_Request $request       REST Request object.
+ */
+function retroliens_rest_pre_insert_filter( $prepared_post, $request ) {
+	$to_ping = $request->get_param( 'to_ping' );
+
+	if ( $to_ping ) {
+		$prepared_post->to_ping = retroliens_prepare_trackbacks( $to_ping );
+	}
+
+	$pinged = $request->get_param( 'pinged' );
+
+	if ( $pinged ) {
+		$prepared_post->pinged = retroliens_prepare_trackbacks( $pinged );
+	}
+
+	return $prepared_post;
+}
+
+/**
+ * Uses the Post Attributes to save the trackbacks.
+ *
+ * @since 1.0.0
+ */
+function retroliens_rest_prepare_item_for_database_filters() {
+	$post_types = get_post_types_by_support( 'trackbacks' );
+
+	foreach ( $post_types as $post_type ) {
+		add_filter( "rest_pre_insert_{$post_type}", 'retroliens_rest_pre_insert_filter', 10, 2 );
+	}
+}
+add_action( 'init', 'retroliens_rest_prepare_item_for_database_filters', 1000 );
 
 /**
  * Registers the `to_ping` and `pinged` fields.
@@ -91,7 +155,7 @@ function retroliens_register_rest_fields() {
 		'post',
 		'to_ping',
 		array(
-			'get_callback' => 'retroliens_get_to_ping',
+			'get_callback' => 'retroliens_get_trackbacks',
 			'schema'       => array(
 				'description'       => __( 'Liste des rétroliens à notifier', 'retroliens' ),
 				'type'              => 'array',
@@ -101,7 +165,26 @@ function retroliens_register_rest_fields() {
 				),
 				'default'           => array(),
 				'context'           => array( 'edit' ),
-				'sanitize_callback' => 'retroliens_sanitize_to_ping',
+				'sanitize_callback' => 'retroliens_sanitize_trackbacks',
+			),
+		)
+	);
+
+	register_rest_field(
+		'post',
+		'pinged',
+		array(
+			'get_callback' => 'retroliens_get_trackbacks',
+			'schema'       => array(
+				'description'       => __( 'Liste des rétroliens notifiés', 'retroliens' ),
+				'type'              => 'array',
+				'items'             => array(
+					'type'   => 'string',
+					'format' => 'uri',
+				),
+				'default'           => array(),
+				'context'           => array( 'edit' ),
+				'sanitize_callback' => 'retroliens_sanitize_trackbacks',
 			),
 		)
 	);
--- src/index.js
+++ src/index.js
@@ -3,63 +3,270 @@ const { PluginSidebar } = wp.editPost;
 const { createElement, Component } = wp.element;
 const { __ } = wp.i18n;
 const { withSelect, withDispatch } = wp.data;
-const { Panel, PanelBody, PanelRow } = wp.components;
+const {
+	Panel,
+	PanelBody,
+	PanelRow,
+	TextControl,
+	ExternalLink,
+	Button,
+	Dashicon,
+} = wp.components;
 const { compose } = wp.compose;
 
-function SendTrackbacks( { post } ) {
-	console.log( post );
-	return (
-		<p>
-			{ __( 'Placeholder for the textarea', 'retroliens' ) }
-		</p>
-	);
-};
+const { filterURLForDisplay } = wp.url;
+const { indexOf, pull, head } = lodash;
+
+class SendTrackbacks extends Component {
+    constructor() {
+		super( ...arguments );
+
+        this.state = {
+			trackback: '',
+			trackbacks: [],
+		};
+	}
+
+	addTrackback( e ) {
+		e.preventDefault();
+
+		const { trackbacks, trackback } = this.state;
+		const { onAddTrackBack } = this.props;
+		let allTrackbacks = trackbacks;
+
+		if ( trackback ) {
+			allTrackbacks = [ ...allTrackbacks, trackback ];
+			this.setState( {
+				trackback: '',
+				trackbacks: allTrackbacks,
+			} );
+
+			return onAddTrackBack( allTrackbacks );
+		}
+	}
+
+	render() {
+		const { trackback, trackbacks } = this.state;
+
+		return (
+			<div>
+				<TextControl
+					label={ __( 'URL de destination', 'retroliens' ) }
+					value={ trackback }
+					type='url'
+					onChange={ ( url ) => {
+						this.setState( { trackback: url } );
+					} }
+				/>
+				<Button isPrimary={ true } isLarge={ true } onClick={ ( e ) => this.addTrackback( e ) }>
+					{ __( 'Ajouter', 'risk-ops' ) }
+				</Button>
+				<p className="description">
+					<Dashicon icon="info" /> { __( 'Un rétrolien est une manière de notifier les anciens systèmes de blog que vous avez fait un lien vers eux. Si vous faites des liens vers des sites WordPress, ils seront notifiés automatiquement à l’aide des pings, sans que vous n’ayez rien à faire.', 'retroliens' ) }
+					<br/>
+					<ExternalLink href="https://wordpress.org/support/article/introduction-to-blogging/#comments" hrefLang="en">
+						En savoir plus sur les pings (en).
+					</ExternalLink>
+				</p>
+			</div>
+		);
+	}
+}
+
+const AddTrackbacks = compose( [
+	withDispatch( ( dispatch ) => ( {
+		onAddTrackBack( trackbacks ) {
+			dispatch( 'core/editor' ).editPost( {
+				to_ping: trackbacks,
+			} );
+		},
+	} ) ),
+] )( SendTrackbacks );
+
+class ManageTrackbacks extends Component {
+    constructor() {
+		super( ...arguments );
+
+        this.state = {
+			trackbacks: [],
+			pinged: [],
+		};
+	}
+
+    componentDidMount() {
+		const { trackbacks, pinged } = this.props;
+		let state = {};
+
+		if ( trackbacks ) {
+			state.trackbacks = trackbacks;
+		}
+
+		if ( pinged ) {
+			state.pinged = pinged;
+		}
+
+		this.setState( state );
+	}
+
+	removeTrackback( e, url ) {
+		e.preventDefault();
+
+		const { trackbacks } = this.state;
+		const { onResetTrackBack, onRemoveTrackBack } = this.props;
+		pull( trackbacks, url );
+
+		this.setState( { trackacks: trackbacks } );
+
+		// Makes sure the Update button is refreshed.
+		return onRemoveTrackBack( [... trackbacks, [''] ] );
+	}
+
+	render() {
+		const { trackbacks, pinged } = this.state;
+		let trackbacksList, postLink, linkDisplay;
+
+		if ( ! trackbacks || ! trackbacks.length ) {
+			return null;
+		}
+
+		if ( ! head( trackbacks ) ) {
+			return null;
+		}
+
+		trackbacksList = trackbacks.map( ( url, id ) => {
+			postLink = url.replace( 'wp-trackback.php', '' )
+						  .replace( '/trackback', '' );
+			linkDisplay = filterURLForDisplay( postLink );
+
+			if ( url && -1 === indexOf( pinged, url ) ) {
+				return (
+					<li key={ 'trackback-' + id }>
+						<a href="#" onClick={ ( e ) => this.removeTrackback( e, url ) }>
+							<Dashicon icon="trash" />
+						</a> <a href={ postLink }>{ linkDisplay }</a>
+					</li>
+				);
+			}
+		} );
+
+		return (
+			<Panel>
+				<PanelBody
+					title={ __( 'Rétroliens à envoyer', 'retroliens' ) }
+					initialOpen={ false }
+				>
+					<PanelRow>
+						<ul>{ trackbacksList }</ul>
+					</PanelRow>
+				</PanelBody>
+			</Panel>
+		);
+	}
+}
 
 const TrackbacksToSend = compose( [
 	withSelect( ( select ) => {
 		const post = select( 'core/editor' ).getCurrentPost();
 
 		return {
-			post: post.to_ping
+			trackbacks: post.to_ping,
+			pinged: post.pinged,
 		};
 	} ),
-	/*withDispatch( ( dispatch ) => ( {
-		onUpdate( trackback ) {
+	withDispatch( ( dispatch ) => ( {
+		onRemoveTrackBack( trackbacks ) {
 			dispatch( 'core/editor' ).editPost( {
-				to_ping: { trackback },
+				to_ping: trackbacks[0],
 			} );
 		},
-	} ) ),*/
-] )( SendTrackbacks );
+	} ) ),
+] )( ManageTrackbacks );
+
+class ViewSentTrackbacks extends Component {
+    constructor() {
+		super( ...arguments );
+
+        this.state = {
+			pinged: [],
+		};
+	}
+
+    componentDidMount() {
+		const { pinged } = this.props;
+
+		if ( pinged ) {
+			this.setState( {
+				pinged: pinged,
+			} );
+		}
+	}
+
+	render() {
+		const { pinged } = this.state;
+		let pingedList, postLink, linkDisplay;
+
+		if ( ! pinged || ! pinged.length ) {
+			return null;
+		}
+
+		pingedList = pinged.map( ( url, id ) => {
+			postLink = url.replace( 'wp-trackback.php', '' )
+						  .replace( '/trackback', '' );
+			linkDisplay = filterURLForDisplay( postLink );
+
+			if ( url ) {
+				return (
+					<li key={ 'pinged-' + id }>
+						<Dashicon icon="yes" /> <a href={ postLink }>{ linkDisplay }</a>
+					</li>
+				);
+			}
+		} );
+
+		return (
+			<Panel>
+				<PanelBody
+					title={ __( 'Rétroliens envoyés', 'retroliens' ) }
+					initialOpen={ false }
+				>
+					<PanelRow>
+						<ul>{ pingedList }</ul>
+					</PanelRow>
+				</PanelBody>
+			</Panel>
+		);
+	}
+}
+
+const SentTrackbacks = compose( [
+	withSelect( ( select ) => {
+		const post = select( 'core/editor' ).getCurrentPost();
+
+		return {
+			pinged: post.pinged
+		};
+	} ),
+] )( ViewSentTrackbacks );
 
 function RetroliensSidebar() {
 	return (
 		<PluginSidebar
 			name='retroliens/manage'
-			title={ __( 'Trackbacks', 'retroliens' ) }
+			title={ __( 'Rétroliens', 'retroliens' ) }
 			icon='admin-links'
 		>
 			<Panel>
 				<PanelBody
-					title={ __( 'Envoyer des Trackbacks', 'retroliens' ) }
+					title={ __( 'Envoyer des rétroliens', 'retroliens' ) }
 					initialOpen={ true }
 				>
 					<PanelRow>
-						<p>{ __( 'Trackbacks', 'retroliens' ) }</p>
-						<TrackbacksToSend />
+						<AddTrackbacks />
 					</PanelRow>
 				</PanelBody>
 			</Panel>
-			<Panel>
-				<PanelBody
-					title={ __( 'Gérer les Trackbacks', 'retroliens' ) }
-					initialOpen={ false }
-				>
-					<PanelRow>
-						<p>{ __( 'Trackbacks', 'retroliens' ) }</p>
-					</PanelRow>
-				</PanelBody>
-			</Panel>
+			<TrackbacksToSend />
+			<SentTrackbacks />
 		</PluginSidebar>
 	);
 }
