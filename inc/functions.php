4ce37aa9d3572097cc8f6ef4fe1e2331a5d2ff40
imath <math.viet@gmail.com>
2020-01-09 07:28:11 +0100

    Register the `to_ping` REST field

--- inc/functions.php
+++ inc/functions.php
@@ -38,3 +38,72 @@ function retroliens_enqueue_script() {
 	wp_enqueue_script( 'retroliens' );
 }
 add_action( 'enqueue_block_editor_assets', 'retroliens_enqueue_script' );
+
+/**
+ * Fetches the `to_ping` post field.
+ *
+ * @since 1.0.0
+ *
+ * @param WP_Post $post The post object.
+ * @return array        The list of trackbacks to ping.
+ */
+function retroliens_get_to_ping( $post ) {
+	$to_ping    = array();
+	$trackbacks = get_post_field( 'to_ping', $post->ID );
+
+	if ( $trackbacks ) {
+		$urls    = trim( $trackbacks, "\n" );
+		$to_ping = explode( "\n", $urls );
+	}
+
+	return $to_ping;
+}
+
+/**
+ * Sanitizes the `to_ping` field.
+ *
+ * @since 1.0.0
+ *
+ * @param string|array $trackbacks The trackbacks to ping for the post.
+ * @return string                  The sanitized trackbacks to ping for the post.
+ */
+function retroliens_sanitize_to_ping( $trackbacks ) {
+	$to_ping = '';
+
+	if ( ! $trackbacks ) {
+		return $to_ping;
+	}
+
+	if ( is_array( $trackbacks ) ) {
+		$to_ping = implode( ' ', $trackbacks );
+	}
+
+	return sanitize_trackback_urls( $to_ping );
+}
+
+/**
+ * Registers the `to_ping` and `pinged` fields.
+ *
+ * @since 1.0.0
+ */
+function retroliens_register_rest_fields() {
+	register_rest_field(
+		'post',
+		'to_ping',
+		array(
+			'get_callback' => 'retroliens_get_to_ping',
+			'schema'       => array(
+				'description'       => __( 'Liste des rétroliens à notifier', 'retroliens' ),
+				'type'              => 'array',
+				'items'             => array(
+					'type'   => 'string',
+					'format' => 'uri',
+				),
+				'default'           => array(),
+				'context'           => array( 'edit' ),
+				'sanitize_callback' => 'retroliens_sanitize_to_ping',
+			),
+		)
+	);
+}
+add_action( 'rest_api_init', 'retroliens_register_rest_fields' );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { PluginSidebar } = wp.editPost;
 const { createElement, Component } = wp.element;
 const { __ } = wp.i18n;
 const { withSelect, withDispatch } = wp.data;
-const { Panel, PanelBody, PanelRow, G, Path, SVG } = wp.components;
+const { Panel, PanelBody, PanelRow } = wp.components;
 const { compose } = wp.compose;
 
 function SendTrackbacks( { post } ) {
@@ -20,7 +20,7 @@ const TrackbacksToSend = compose( [
 		const post = select( 'core/editor' ).getCurrentPost();
 
 		return {
-			post: post
+			post: post.to_ping
 		};
 	} ),
 	/*withDispatch( ( dispatch ) => ( {
@@ -32,33 +32,12 @@ const TrackbacksToSend = compose( [
 	} ) ),*/
 ] )( SendTrackbacks );
 
-function RetroliensSidebarIcon() {
-	return (
-		<SVG
-			aria-hidden='true'
-			role='img'
-			className='dashicon trackback-icon'
-			focusable='false'
-			width='20'
-			height='20'
-			viewBox='0 0 100 100'
-			xmlns='http://www.w3.org/2000/svg'
-		>
-			<G>
-				<Path d="M82.25,69.742H16.74c-0.485,1.293-2.015,2.41-2.712,2.865v0.221V79H85v-6.172v-0.209  C84.285,72.176,82.695,71.066,82.25,69.742L82.25,69.742z"/>
-				<Path d="M55,27H44.025c-1.104,0-2-0.896-2-2s0.896-2,2-2H55c1.104,0,2,0.896,2,2S56.104,27,55,27z"/>
-				<Path d="M83.154,63.322c-0.824-0.417-1.754-1.207-1.836-1.562c-1.752-12.246-6.957-28.546-31.805-28.546  c-24.849,0-30.055,16.3-31.81,28.562c-0.079,0.345-1.022,1.146-1.833,1.549c-0.833,0.41-1.271,1.283-1.06,2.189  C15.023,66.418,15.83,67,16.759,67h65.509c0.931,0,1.735-0.582,1.947-1.486C84.427,64.607,83.986,63.734,83.154,63.322z   M38.334,44.196c-2.81,1.465-5.334,3.249-7.503,5.301c-0.387,0.366-0.881,0.547-1.375,0.547c-0.53,0-1.06-0.209-1.453-0.625  c-0.759-0.802-0.725-2.068,0.078-2.828c2.438-2.307,5.265-4.307,8.404-5.943c0.978-0.509,2.188-0.131,2.698,0.849  C39.694,42.478,39.313,43.686,38.334,44.196z"/>
-			</G>
-		</SVG>
-	);
-}
-
 function RetroliensSidebar() {
 	return (
 		<PluginSidebar
 			name='retroliens/manage'
 			title={ __( 'Trackbacks', 'retroliens' ) }
-			icon={ RetroliensSidebarIcon }
+			icon='admin-links'
 		>
 			<Panel>
 				<PanelBody
